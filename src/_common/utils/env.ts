8f4e8d1e8149589d8ead05a0347003c6b37c33fa
hesham1221 <hishammohmed12345@gmail.com>
2023-01-14 22:36:35 +0200

    Fix : env file while building

--- src/_common/utils/env.ts
+++ src/_common/utils/env.ts
@@ -1,5 +1,3 @@
-import * as dotenv from 'dotenv';
-import * as fs from 'fs';
-import * as path from 'path';
 
-export const env = dotenv.parse(fs.readFileSync(path.join(process.cwd(), '.env')));
+
+export const env = process.env
