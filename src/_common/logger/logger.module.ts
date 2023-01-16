750584fd0a3664b99b6684677a41536864b35d25
hesham1221 <hishammohmed12345@gmail.com>
2023-01-17 01:43:03 +0200

    fix : problem directory not found with the logger module in the production mode

--- src/_common/logger/logger.module.ts
+++ src/_common/logger/logger.module.ts
@@ -14,6 +14,9 @@ import { ConfigService } from '@nestjs/config';
       providers: [ConfigService],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => {
+        if (!fs.existsSync(path.join(process.cwd(), 'logs'))) {
+          fs.mkdirSync(path.join(process.cwd(), 'logs'));
+        }
         return {
           pinoHttp: [
             {
@@ -29,40 +32,45 @@ import { ConfigService } from '@nestjs/config';
                   options: {
                     colorize: true,
                     levelFirst: true,
-                    crlf: true
-                  }
-                }
+                    crlf: true,
+                  },
+                },
               }),
               serializers: {
                 err: pino.stdSerializers.err,
                 req: pino.stdSerializers.req,
-                res: pino.stdSerializers.res
+                res: pino.stdSerializers.res,
               },
-              useLevel: config.get('NODE_ENV') !== 'production' ? 'debug' : 'info',
-              genReqId: req => req.id || uuid.v4(),
+              useLevel:
+                config.get('NODE_ENV') !== 'production' ? 'debug' : 'info',
+              genReqId: (req) => req.id || uuid.v4(),
               autoLogging: false,
               ...(config.get('NODE_ENV') === 'production' && {
                 logger: pino(
                   { level: 'debug' },
                   pino.multistream([
                     { stream: process.stdout },
-                    { stream: fs.createWriteStream(path.join(process.cwd(), 'logs/logs.out')) },
+                    {
+                      stream: fs.createWriteStream(
+                        path.join(process.cwd(), 'logs/logs.out'),
+                      ),
+                    },
                     {
                       stream: createWriteStream({
                         dsn: config.get('SENTRY_DNS'),
-                        serverName: config.get('SERVER_NAME')
-                      })
-                    }
-                  ])
-                )
-              })
+                        serverName: config.get('SERVER_NAME'),
+                      }),
+                    },
+                  ]),
+                ),
+              }),
             },
-            process.stdout
-          ]
+            process.stdout,
+          ],
         };
-      }
-    })
+      },
+    }),
   ],
-  exports: [LoggerModule]
+  exports: [LoggerModule],
 })
 export class LoggerModule {}
