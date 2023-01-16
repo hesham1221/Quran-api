7db3b62d09316fe2e1c76bd7024f747a6d64a975
Hesham Mohamed <89036077+hesham1221@users.noreply.github.com>
2023-01-17 01:49:11 +0200

    Update env.ts

--- src/_common/utils/env.ts
+++ src/_common/utils/env.ts
@@ -2,4 +2,5 @@ import * as dotenv from 'dotenv';
 import * as fs from 'fs';
 import * as path from 'path';
 
-export const env = dotenv.parse(fs.readFileSync(path.join(process.cwd(), '.env')));
+
+export const env = process.env.NODE_ENV === 'production' ? process.env : dotenv.parse(fs.readFileSync(path.join(process.cwd(), '.env')));
